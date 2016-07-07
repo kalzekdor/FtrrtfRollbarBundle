730a7cb46eba5500a1c82a7d976b1b5f8fd67f9d
feamsr00 <feamsr00@feamsternet.net>
2016-07-06 21:42:53 -0400

    Update UserHelper.php

--- Helper/UserHelper.php
+++ Helper/UserHelper.php
@@ -13,31 +13,22 @@ class UserHelper
      */
     public function buildUserData($user)
     {
-        var_dump(">>>>>>>>>>>>>>>>>>>>>C H E C K P O I N T R E A C H E D<<<<<<<<<<<<<<<<:".sprintf("%s|%s",__CLASS__, __LINE__));
-        var_dump(get_class($user));
         $userData = array();
-        var_dump(">>>>>>>>>>>>>>>>>>>>>C H E C K P O I N T R E A C H E D<<<<<<<<<<<<<<<<:".sprintf("%s|%s",__CLASS__, __LINE__));
         if(method_exists($user, 'getId'))
         {
-            var_dump(">>>>>>>>>>>>>>>>>>>>>C H E C K P O I N T R E A C H E D<<<<<<<<<<<<<<<<:".sprintf("%s|%s",__CLASS__, __LINE__));
             $userData['id']=$user->getUsername();  
         }
         else
         {
-            var_dump(">>>>>>>>>>>>>>>>>>>>>C H E C K P O I N T R E A C H E D<<<<<<<<<<<<<<<<:".sprintf("%s|%s",__CLASS__, __LINE__));
             $userData['id']=(string) $user;
         }
          // = method_exists($user, 'getId')
          //    ? 
          //    : 
-        var_dump(">>>>>>>>>>>>>>>>>>>>>C H E C K P O I N T R E A C H E D<<<<<<<<<<<<<<<<:".sprintf("%s|%s",__CLASS__, __LINE__));        
         $userData['username'] = (string) $user;
-        var_dump(">>>>>>>>>>>>>>>>>>>>>C H E C K P O I N T R E A C H E D<<<<<<<<<<<<<<<<:".sprintf("%s|%s",__CLASS__, __LINE__));
         if (method_exists($user, 'getEmail')) {
             $userData['email'] = $user->getEmail();
         }
-
-        var_dump(">>>>>>>>>>>>>>>>>>>>>C H E C K P O I N T R E A C H E D<<<<<<<<<<<<<<<<:".sprintf("%s|%s",__CLASS__, __LINE__));
         return $userData;
     }
 }
